3ad05e0e209f05e1fcc53497c9fe7d88c166309d
DanielStarchl <d.starchl-barbu@students.htl-leonding.ac.at>
2026-04-22 09:38:27 +0200

    ui implementation + database structure setup

---
+++ project/app/database.php
@@ -0,0 +1,17 @@
+<?php
+
+$_db_host = "db_server";
+$_db_username = "root";
+$_db_password = "rootpassword";
+$_db_datenbank = "Playerstate";
+
+$conn = new mysqli($_db_host,
+                   $_db_username,
+                   $_db_password,
+                   $_db_datenbank);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
